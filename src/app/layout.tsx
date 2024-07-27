6e938e067cd7082144a5e3c95d3febb2609fe768
abdul-haseeb123 <syedabdulhaseeb611@gmail.com>
2024-07-27 18:45:23 +0500

    added fix for favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/product-list-cart-next/favicon.png"
+        />
+      </head>
       <body className="font-redhat">
         <ProductsProvider>{children}</ProductsProvider>
       </body>
